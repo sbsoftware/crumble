8e4d630d33773eaea1cb5f892ab21654a96224e4
Stefan Bilharz <stefan@sbsoftware.de>
2021-12-08 21:50:09 +0100

    WIP: Layouts and nested Views

--- test.cr
+++ test.cr
@@ -1,53 +1,50 @@
 module BuildContext
-  TAG_METHODS = ["div", "strong"]
+  TAG_METHODS = ["html", "head", "title", "body", "div", "strong"]
 
   macro capture_elems(&blk)
     {% if blk %}
       {% if blk.body.is_a?(Expressions) %}
         {% for exp in blk.body.expressions %}
-          {% if exp.is_a?(Call) %}
-            {% if TAG_METHODS.includes?(exp.name.stringify) %}
-              {% if exp.block %}
-                {{exp.name}}({{exp.args.splat}}) do
-                  BuildContext.capture_elems {{exp.block}}
-                end
-              {% else %}
-                {{exp.name}}({{exp.args.splat}})
-              {% end %}
-            {% else %}
-              __tplio__ << {{exp.name}}({{exp.args.splat}}) {{exp.block}}
-            {% end %}
-          {% elsif exp.is_a?(StringLiteral) %}
-            __tplio__ << {{exp + "\n"}}
-          {% end %}
+          BuildContext.eval_exp do
+            {{exp}}
+          end
         {% end %}
       {% else %}
-        {% if blk.body.is_a?(Call) %}
-          {% if TAG_METHODS.includes?(blk.body.name.stringify) %}
-            {% if blk.body.block %}
-              {{blk.body.name}}({{blk.body.args.splat}}) do
-                BuildContext.capture_elems {{blk.body.block}}
-              end
-            {% else %}
-              {{blk.body.name}}({{blk.body.args.splat}})
-            {% end %}
-          {% else %}
-            __tplio__ << {{blk.body.name}}({{blk.body.args.splat}}) {{blk.body.block}}
-          {% end %}
-        {% elsif blk.body.is_a?(StringLiteral) %}
-          __tplio__ << {{blk.body + "\n"}}
+        BuildContext.eval_exp {{blk}}
+      {% end %}
+    {% end %}
+    {{debug}}
+  end
+
+  macro eval_exp(&blk)
+    {% if blk.body.is_a?(Call) %}
+      {% if TAG_METHODS.includes?(blk.body.name.stringify) %}
+        {% if blk.body.block %}
+          {{blk.body.name}}({{blk.body.args.splat}}) do
+            BuildContext.capture_elems {{blk.body.block}}
+          end
+        {% else %}
+          {{blk.body.name}}({{blk.body.args.splat}})
+        {% end %}
+      {% else %}
+        {% if blk.body.receiver %}
+          __tplio__ << {{blk.body.receiver}}.{{blk.body.name}}({{blk.body.args.splat}}) {{blk.body.block}}
+        {% else %}
+          __tplio__ << {{blk.body.name}}({{blk.body.args.splat}}) {{blk.body.block}}
         {% end %}
       {% end %}
+    {% elsif blk.body.is_a?(StringLiteral) %}
+      __tplio__ << {{blk.body + "\n"}}
+    {% else %}
+      {{pp blk.body}}
     {% end %}
   end
 end
 
-class View(T)
-  @model : T
-
-  forward_missing_to @model
-
-  def initialize(@model)
+class Template
+  # default implementation to calm down the compiler
+  def render
+    ""
   end
 
   macro template(&blk)
@@ -56,6 +53,22 @@ class View(T)
         BuildContext.capture_elems {{blk}}
       end
     end
+  end
+
+  macro html(&block)
+    tag(__tplio__, "html", false, nil) {{block}}
+  end
+
+  macro head(&block)
+    tag(__tplio__, "head", false, nil) {{block}}
+  end
+
+  macro title(&block)
+    tag(__tplio__, "title", false, nil) {{block}}
+  end
+
+  macro body(css_class = nil, &block)
+    tag(__tplio__, "body", false, {{css_class}}) {{block}}
   end
 
   macro div(css_class = nil, &block)
@@ -105,6 +118,15 @@ class View(T)
   end
 end
 
+class View(T) < Template
+  @model : T
+
+  forward_missing_to @model
+
+  def initialize(@model)
+  end
+end
+
 class MyData
   getter theprop : String
 
@@ -143,6 +165,25 @@ class MyView(T) < View(T)
   end
 end
 
+class MyLayout(T) < View(T)
+  template do
+    html do
+      head do
+        title do
+          site_title
+        end
+      end
+      body do
+        site_body.render
+      end
+    end
+  end
+end
+
+record SiteStructure, site_title : String, site_body : Template
+
 puts MyView(MyData).new(MyData.new("PIMMEL")).render
 puts "#####"
 puts MyView(MyOtherData).new(MyOtherData.new("Suburu", "geneter")).render
+puts "#####"
+puts MyLayout(SiteStructure).new(SiteStructure.new("3 TAGE WACH", MyView(MyData).new(MyData.new("IMPORANT DATA")))).render
