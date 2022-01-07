3c48fb1b9b054ac8694df6ced9ccd64f3a24819c
Stefan Bilharz <stefan@sbsoftware.de>
2022-01-07 21:07:38 +0100

    Also allow Float64 percent values

--- src/css/percent.cr
+++ src/css/percent.cr
@@ -1,12 +1,20 @@
-record Percent, value : Int32 do
+record Percent, value : (Int32 | Float64) do
   def to_s(io : IO)
     io << value
     io << "%"
   end
 end
 
-struct Int32
+module PercentInclude
   def percent
     Percent.new(self)
   end
+end
+
+struct Int32
+  include PercentInclude
+end
+
+struct Float64
+  include PercentInclude
 end
