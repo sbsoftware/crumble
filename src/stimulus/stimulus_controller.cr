15acbb5efca76f5bdbba58cabce4f5cb911e9814
Stefan Bilharz <stefan@sbsoftware.de>
2022-02-04 20:54:45 +0100

    Add stimulus controller values

--- src/stimulus/stimulus_controller.cr
+++ src/stimulus/stimulus_controller.cr
@@ -12,152 +12,189 @@ end
 class ClickEvent < JavascriptEvent
 end
 
-record Target, controller : StimulusController.class, name : String do
-  def html_attr_key
-    "data-#{controller.controller_name}-target"
-  end
-
-  def html_attr_value(io)
-    io << name
-  end
-end
-
-class Targets
-  @arr = [] of Target
-
-  def <<(target)
-    @arr << target
-  end
-
-  def to_s(io : IO)
-    io << "  static targets = ["
-    io << @arr.map do |target|
-      "\"#{target.name}\""
-    end.join(", ")
-    io << "]\n"
-  end
-end
-
-record Method, name : String, body : String do
-  def to_s(io : IO)
-    io << "  "
-    io << name
-    io << "() {\n"
-    io << body
-    io << "  }"
-  end
-end
-
-class Methods
-  @methods = {} of String => Method
-
-  def <<(method)
-    @methods[method.name] = method
-  end
-
-  def to_s(io : IO)
-    io << @methods.values.map(&.to_s).join("\n\n")
-  end
-end
-
-record Action, event : JavascriptEvent.class, controller : StimulusController.class, method : Method do
-  def html_attr_key
-    "data-action"
-  end
-
-  def html_attr_value(io)
-    io << event
-    io << "->"
-    io << controller.controller_name
-    io << "#"
-    io << method.name
-  end
-end
-
-abstract class CallContext
-  @receiver : String
-
-  def initialize(@receiver = "")
-  end
-
-  def resolve_call(name, *args)
-    "#{receiver_dot(name)}(#{args.join(", ")})"
-  end
-
-  def resolve_attr(name)
-    receiver_dot(name)
-  end
-
-  def forward(ctx_class, next_receiver)
-    ctx_class.new(receiver_dot(next_receiver))
-  end
-
-  def to_s(io : IO)
-    io << @receiver
-  end
-
-  def inspect(io : IO)
-    io << @receiver
-  end
-
-  def receiver_dot(call)
-    @receiver.blank? ? call : "#{@receiver}.#{call}"
-  end
-end
-
-# exists to write JS null from within a crystal hash
-class NullContext
-  def self.inspect(io : IO)
-    io << "null"
-  end
-end
-
-class GeneralMethodContext < CallContext
-  def window
-    forward(ElementContext, "window")
-  end
-
-  def console
-    forward(ConsoleContext, "console")
-  end
-end
-
-class GeneralControllerContext < CallContext
-  def dispatch(event : JavascriptEvent.class, target : ElementContext)
-    resolve_call("dispatch", event.to_s.dump, {target: target, prefix: NullContext})
-  end
-end
-
-class ConsoleContext < CallContext
-  alias Loggable = String
-
-  def log(*entries : Loggable)
-    resolve_call("log", *entries)
-  end
-end
-
-class ElementContext < CallContext
-  def value
-    resolve_attr("value")
-  end
-
-  def innerHTML
-    resolve_attr("innerHTML")
-  end
-
-  def classList
-    forward(ClassListContext, "classList")
-  end
-end
-
-class ClassListContext < CallContext
-  def toggle(klass : CSS::CSSClass.class)
-    resolve_call("toggle", klass.to_s.dump)
-  end
-end
-
 abstract class StimulusController
+  record Target, controller : StimulusController.class, name : String do
+    def html_attr_key
+      "data-#{controller.controller_name}-target"
+    end
+
+    def html_attr_value(io)
+      io << name
+    end
+  end
+
+  class Targets
+    @arr = [] of Target
+
+    def <<(target)
+      @arr << target
+    end
+
+    def to_s(io : IO)
+      io << "  static targets = ["
+      io << @arr.map do |target|
+        "\"#{target.name}\""
+      end.join(", ")
+      io << "]\n"
+    end
+  end
+
+  record Value, controller : StimulusController.class, name : String, value : String do
+    def html_attr_key
+      "data-#{controller.controller_name}-#{name}-value"
+    end
+
+    def html_attr_value(io)
+      io << value
+    end
+  end
+
+  class Values
+    @arr = [] of String
+
+    def <<(value)
+      @arr << value
+    end
+
+    def to_s(io : IO)
+      io << "  static values = {"
+      io << @arr.map do |value|
+        "#{value}: String"
+      end.join(", ")
+      io << "}\n"
+    end
+  end
+
+  record Method, name : String, body : String do
+    def to_s(io : IO)
+      io << "  "
+      io << name
+      io << "() {\n"
+      io << body
+      io << "  }"
+    end
+  end
+
+  class Methods
+    @methods = {} of String => Method
+
+    def <<(method)
+      @methods[method.name] = method
+    end
+
+    def to_s(io : IO)
+      io << @methods.values.map(&.to_s).join("\n\n")
+    end
+  end
+
+  record Action, event : JavascriptEvent.class, controller : StimulusController.class, method : Method do
+    def html_attr_key
+      "data-action"
+    end
+
+    def html_attr_value(io)
+      io << event
+      io << "->"
+      io << controller.controller_name
+      io << "#"
+      io << method.name
+    end
+  end
+
+  abstract class CallContext
+    @receiver : String
+
+    def initialize(@receiver = "")
+    end
+
+    def resolve_call(name, *args)
+      "#{receiver_dot(name)}(#{args.join(", ")})"
+    end
+
+    def resolve_attr(name)
+      receiver_dot(name)
+    end
+
+    def forward(ctx_class, next_receiver)
+      ctx_class.new(receiver_dot(next_receiver))
+    end
+
+    def forward_call(ctx_class, next_receiver, *args)
+      ctx_class.new(resolve_call(next_receiver, *args))
+    end
+
+    def to_s(io : IO)
+      io << @receiver
+    end
+
+    def inspect(io : IO)
+      io << @receiver
+    end
+
+    def receiver_dot(call)
+      @receiver.blank? ? call : "#{@receiver}.#{call}"
+    end
+  end
+
+  # exists to write JS null from within a crystal hash
+  class NullContext
+    def self.inspect(io : IO)
+      io << "null"
+    end
+  end
+
+  class GeneralMethodContext < CallContext
+    def window
+      forward(ElementContext, "window")
+    end
+
+    def console
+      forward(ConsoleContext, "console")
+    end
+  end
+
+  end
+
+  class GeneralControllerContext < CallContext
+    def dispatch(event : JavascriptEvent.class, target : ElementContext)
+      resolve_call("dispatch", event.to_s.dump, {target: target, prefix: NullContext})
+    end
+  end
+
+  class ConsoleContext < CallContext
+    alias Loggable = String
+
+    def log(*entries : Loggable)
+      resolve_call("log", *entries)
+    end
+  end
+
+  class ElementContext < CallContext
+    def value
+      resolve_attr("value")
+    end
+
+    def innerHTML
+      resolve_attr("innerHTML")
+    end
+
+    def classList
+      forward(ClassListContext, "classList")
+    end
+
+    def dataset
+      resolve_attr("dataset")
+    end
+  end
+
+  class ClassListContext < CallContext
+    def toggle(klass : CSS::CSSClass.class)
+      resolve_call("toggle", klass.to_s.dump)
+    end
+  end
+
   @@targets = Targets.new
+  @@values = Values.new
   @@methods = Methods.new
 
   def self.to_s(io : IO)
@@ -165,6 +202,7 @@ abstract class StimulusController
     io << controller_name
     io << "\", class extends Controller {\n"
     io << @@targets
+    io << @@values
     io << @@methods
     io << "\n})"
   end
@@ -218,9 +256,25 @@ abstract class StimulusController
     {% end %}
   end
 
+  macro values(*values)
+    {% for value_name in values %}
+      def self.{{value_name.id}}_value(value)
+        Value.new(self, {{value_name.id.stringify}}, value)
+      end
+
+      private class ControllerContext
+        def {{value_name.camelcase(lower: true).id}}Value
+          resolve_attr("{{value_name.id}}Value")
+        end
+      end
+
+      @@values << {{value_name.id.stringify}}
+    {% end %}
+  end
+
   macro method(name, &blk)
     @@{{name.id}}_method = Method.new(
-      name: {{name}},
+      name: {{name.id.stringify}},
       body: String.build do |codeio|
         capture_code ControllerMethodContext {{blk}}
       end
