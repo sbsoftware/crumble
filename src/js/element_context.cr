e68ffd68b5d2f280dd38fea8edbebd0244e0ec52
Stefan Bilharz <stefan@sbsoftware.de>
2023-04-12 22:57:46 +0200

    Add more methods to ElementContext

--- src/js/element_context.cr
+++ src/js/element_context.cr
@@ -3,12 +3,28 @@ require "./class_list_context"
 
 module JS
   class ElementContext < CallContext
+    def ==(other)
+      "#{@receiver} === #{other}"
+    end
+
+    def !=(other)
+      "#{@receiver} != #{other}"
+    end
+
     def value
-      resolve_attr("value")
+      forward(StringContext, "value")
+    end
+
+    def value=(new_value)
+      resolve_assignment("value", new_value)
     end
 
     def innerHTML
       resolve_attr("innerHTML")
+    end
+
+    def innerHTML=(new_html)
+      resolve_assignment("innerHTML", new_html)
     end
 
     def outerHTML=(new_html)
@@ -25,6 +41,14 @@ module JS
 
     def click
       resolve_call("click")
+    end
+
+    def children
+      ArrayContext(ElementContext).new("Array.from(#{receiver_dot("children")})")
+    end
+
+    def textContent
+      forward(StringContext, "textContent")
     end
   end
 end
