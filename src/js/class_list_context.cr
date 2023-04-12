03be0194fa9426e1ac4d6b9b0a2e60d5f1b5c2b2
Stefan Bilharz <stefan@sbsoftware.de>
2023-04-12 22:57:01 +0200

    Add #add and #remove to ClassListContext

--- src/js/class_list_context.cr
+++ src/js/class_list_context.cr
@@ -2,6 +2,14 @@ require "./call_context"
 
 module JS
   class ClassListContext < CallContext
+    def add(klass : CSS::CSSClass.class)
+      resolve_call("add", klass.to_s.dump)
+    end
+
+    def remove(klass : CSS::CSSClass.class)
+      resolve_call("remove", klass.to_s.dump)
+    end
+
     def toggle(klass : CSS::CSSClass.class)
       resolve_call("toggle", klass.to_s.dump)
     end
