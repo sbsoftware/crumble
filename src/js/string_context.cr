daf643e1bc688f5e980db411c08881577c18eb87
Stefan Bilharz <stefan@sbsoftware.de>
2023-04-12 22:58:11 +0200

    Add more methods to JS::StringContext

--- src/js/string_context.cr
+++ src/js/string_context.cr
@@ -5,5 +5,21 @@ module JS
     def ===(other)
       "#{@receiver} === #{other}"
     end
+
+    def includes(other)
+      "#{@receiver}.includes(#{other})"
+    end
+
+    def toLowerCase
+      forward_call(StringContext, "toLowerCase")
+    end
+
+    def toUpperCase
+      forward_call(StringContext, "toUpperCase")
+    end
+
+    def trim
+      forward_call(StringContext, "trim")
+    end
   end
 end
