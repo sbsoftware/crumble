09bc4873b5f8558c09cd6e0bacf53abb2e6aa75c
Stefan Bilharz <stefan@sbsoftware.de>
2024-02-08 20:19:23 +0100

    CR-38 Use "for" attribute for label tags in ElementIds

--- src/css/element_id.cr
+++ src/css/element_id.cr
@@ -8,8 +8,13 @@ module CSS
       CSS::IdSelector.new(self)
     end
 
-    def self.to_html_attrs(_tag, attrs)
-      attrs["id"] = self.to_s
+    def self.to_html_attrs(tag, attrs)
+      case tag
+      when "label"
+        attrs["for"] = self.to_s
+      else
+        attrs["id"] = self.to_s
+      end
     end
   end
 end
