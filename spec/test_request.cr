779ea1dca7e9fd0f8de0887b9f08b0b660d082e9
Stefan Bilharz <stefan@sbsoftware.de>
2024-08-03 20:35:46 +0200

    CR-69 Support supplying body to Crumble::Server::TestRequest

--- spec/test_request.cr
+++ spec/test_request.cr
@@ -4,9 +4,10 @@ class Crumble::Server::TestRequest < HTTP::Request
   getter method : String
   getter headers : HTTP::Headers
 
-  def initialize(id = nil, resource = "/", method = "GET", headers = nil, remote_address : String = "127.0.0.1")
+  def initialize(id = nil, resource = "/", method = "GET", headers = nil, remote_address : String = "127.0.0.1", body : String? = nil)
     @id = id if id
     @remote_address = Socket::IPAddress.new(remote_address, 80)
-    super(method: method, resource: resource, headers: headers, internal: nil)
+    body_io = IO::Memory.new.tap { |io| io << body; io.rewind } if body
+    super(method: method, resource: resource, headers: headers, body: body_io, internal: nil)
   end
 end
