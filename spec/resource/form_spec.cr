50f2efec9cbb1fb8a417ad8ccdd6635217bbfa0b
Stefan Bilharz <stefan@sbsoftware.de>
2025-07-07 22:34:11 +0200

    CR-82 Add Crumble::Form base class

---
+++ spec/resource/form_spec.cr
@@ -0,0 +1,42 @@
+require "../spec_helper"
+
+module Crumble::Resource::FormSpec
+  class TestForm < Crumble::Form
+    field name : String
+    field description : String?
+  end
+
+  class TestResource < Crumble::Resource
+    def create
+      form = TestForm.from_http_params(ctx.request.body)
+
+      if form.valid?
+        ctx.response << "Fields: #{form.values}"
+      else
+        ctx.response << "Errors: #{form.errors}"
+      end
+    end
+  end
+
+  describe "TestResource" do
+    it "should respond to valid input" do
+      res = String.build do |io|
+        ctx = Crumble::Server::TestRequestContext.new(response_io: io, resource: TestResource.uri_path, method: "POST", body: URI::Params.encode({name: "Foo"}))
+        TestResource.handle(ctx)
+        ctx.response.flush
+      end
+
+      res.should contain("Fields: {name: \"Foo\", description: nil}")
+    end
+
+    it "should respond to invalid input" do
+      res = String.build do |io|
+        ctx = Crumble::Server::TestRequestContext.new(response_io: io, resource: TestResource.uri_path, method: "POST", body: URI::Params.encode({description: "Foo"}))
+        TestResource.handle(ctx)
+        ctx.response.flush
+      end
+
+      res.should contain("Errors: [\"name\"]")
+    end
+  end
+end
---
+++ src/form.cr
@@ -0,0 +1,73 @@
+module Crumble
+  abstract class Form
+    annotation Field; end
+    annotation Nilable; end
+
+    getter errors : Array(String)?
+
+    macro field(type_decl)
+      @[Field]
+      {% if type_decl.type.resolve.nilable? %}
+        @[Nilable]
+      {% end %}
+      getter {{type_decl.var}} : {{type_decl.type}}?
+    end
+
+    def self.from_http_params(body : IO?)
+      new(__http_params: body.try(&.gets_to_end) || "")
+    end
+
+    def self.new(*, __http_params : String)
+      instance = allocate
+      instance.initialize(__http_params: __http_params)
+      GC.add_finalizer(instance) if instance.responds_to? :finalize
+      instance
+    end
+
+    def initialize(*, __http_params : String)
+      HTTP::Params.parse(__http_params) do |key, value|
+        {% begin %}
+        case key
+          {% for var in @type.instance_vars.select { |iv| iv.annotation(Field) } %}
+          when {{var.name.stringify}}
+            @{{var.name}} = value
+          {% end %}
+        end
+        {% end %}
+      end
+    end
+
+    def valid?
+      if errors = @errors
+        return errors.none?
+      else
+        errors = @errors = [] of String
+      end
+
+      {% for var in @type.instance_vars.select { |iv| iv.annotation(Field) } %}
+        {% unless var.annotation(Nilable) %}
+          if (%field{var} = @{{var}}).nil?
+            errors << {{var.stringify}}
+          end
+        {% end %}
+      {% end %}
+
+      errors.none?
+    end
+
+    def values
+      {% for var in @type.instance_vars.select { |iv| iv.annotation(Field) && !iv.annotation(Nilable) } %}
+        if (%field{var} = @{{var}}).nil?
+          raise "@{{var}} must not be nil!"
+        end
+      {% end %}
+      {% begin %}
+        {
+          {% for var in @type.instance_vars.select { |iv| iv.annotation(Field) } %}
+            {{var}}: @{{var}},
+          {% end %}
+        }
+      {% end %}
+    end
+  end
+end
