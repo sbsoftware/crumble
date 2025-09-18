7f436297473e5e73860e32f3be896c5ef927056f
Stefan Bilharz <stefan@sbsoftware.de>
2025-09-18 21:42:50 +0200

    CR-94 Allow TestRequestContexts in ViewHandler

--- src/server/view_handler.cr
+++ src/server/view_handler.cr
@@ -4,10 +4,10 @@ module Crumble::Server
   module ViewHandler
     extend Handler
 
-    getter request_ctx : Crumble::Server::RequestContext
+    getter request_ctx : Crumble::Server::RequestContext | Crumble::Server::TestRequestContext
 
-    def ctx
-      @ctx ||= Crumble::Server::HandlerContext.new(request_ctx, self)
+    getter ctx : Crumble::Server::HandlerContext? do
+      Crumble::Server::HandlerContext.new(request_ctx, self)
     end
 
     abstract def window_title : String?
