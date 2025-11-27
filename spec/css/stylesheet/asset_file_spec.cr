afd620417585214239a0b9e405936a48f0177533
Stefan Bilharz <stefan@sbsoftware.de>
2025-11-27 21:37:06 +0100

    CR-83 Add test to verify stylesheet asset files are still working

--- spec/css/stylesheet/asset_file_spec.cr
+++ spec/css/stylesheet/asset_file_spec.cr
@@ -11,5 +11,10 @@ module CSS::Stylesheet::AssetFileSpec
     it "should have the same .uri_path value" do
       MyStyle.uri_path.should match(/\/styles\/css__stylesheet__asset_file_spec__my_style_.+\.css/)
     end
+
+    it "should store the stylesheet contents in the asset file" do
+      asset_file = AssetFileRegistry.query(MyStyle.uri_path).not_nil!
+      asset_file.contents.should eq("div {\n  font-size: 12px;\n}")
+    end
   end
 end
