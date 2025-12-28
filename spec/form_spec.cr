b616dda5f33cf4ccded42195d442672bd7ec6c82
Stefan Bilharz <stefan@sbsoftware.de>
2025-12-28 22:40:42 +0100

    CR-101 Render labels for form field inputs

--- spec/form_spec.cr
+++ spec/form_spec.cr
@@ -14,10 +14,14 @@ class Crumble::FormSpec
   describe "MyForm#to_html" do
     it "should return the correct HTML" do
       expected = <<-HTML.squish
-      <input type="hidden" name="a" value="Blah">
-      <input type="text" name="b" value="">
-      <input type="text" name="c" value="foo">
-      <input type="hidden" name="d" value="true">
+      <label for="crumble--form-spec--my-form--a-field-id">A</label>
+      <input id="crumble--form-spec--my-form--a-field-id" type="hidden" name="a" value="Blah">
+      <label for="crumble--form-spec--my-form--b-field-id">B</label>
+      <input id="crumble--form-spec--my-form--b-field-id" type="text" name="b" value="">
+      <label for="crumble--form-spec--my-form--c-field-id">C</label>
+      <input id="crumble--form-spec--my-form--c-field-id" type="text" name="c" value="foo">
+      <label for="crumble--form-spec--my-form--d-field-id">D</label>
+      <input id="crumble--form-spec--my-form--d-field-id" type="hidden" name="d" value="true">
       HTML
 
       MyForm.new(a: "Blah", b: nil, c: "foo", d: true).to_html.should eq(expected)
