dfcf52160527b7f51aea50bb34aae01ffafcfc9e
Stefan Bilharz <stefan@sbsoftware.de>
2025-12-30 21:09:56 +0100

    CR-104 Make page path matching more explicit

--- spec/page_spec.cr
+++ spec/page_spec.cr
@@ -57,6 +57,8 @@ module Crumble::PageSpec
   end
 
   class IdPage < Crumble::Page
+    path_param id
+
     view do
       template do
         html do
@@ -69,15 +71,89 @@ module Crumble::PageSpec
   end
 
   class NestedPage < Crumble::Page
-    def self.nested_path
-      "/details"
-    end
+    path_param id
+    nested_path "details"
 
     view do
       template do
         html do
           body do
             h1 { "Nested ID: #{ctx.handler.as(Crumble::PageSpec::NestedPage).id}" }
+          end
+        end
+      end
+    end
+  end
+
+  class TopLevelOnlyPage < Crumble::Page
+    view do
+      template do
+        html do
+          body do
+            h1 { "Top level" }
+          end
+        end
+      end
+    end
+  end
+
+  class CustomRootPathPage < Crumble::Page
+    root_path "/custom"
+
+    view do
+      template do
+        html do
+          body do
+            h1 { "Custom root path" }
+          end
+        end
+      end
+    end
+  end
+
+  class MultiParamPage < Crumble::Page
+    root_path "/multi"
+    path_param account_id
+    path_param slug, /[a-z0-9-]+/
+    nested_path "edit"
+    nested_path "details"
+
+    view do
+      template do
+        html do
+          body do
+            page = ctx.handler.as(Crumble::PageSpec::MultiParamPage)
+            h1 { "account_id=#{page.account_id} slug=#{page.slug}" }
+          end
+        end
+      end
+    end
+  end
+
+  class SymbolNameParamPage < Crumble::Page
+    root_path "/sym_param"
+    path_param :id
+
+    view do
+      template do
+        html do
+          body do
+            h1 { "Symbol ID: #{ctx.handler.as(Crumble::PageSpec::SymbolNameParamPage).id}" }
+          end
+        end
+      end
+    end
+  end
+
+  class StringNameParamPage < Crumble::Page
+    root_path "/str_param"
+    path_param "id"
+
+    view do
+      template do
+        html do
+          body do
+            h1 { "String ID: #{ctx.handler.as(Crumble::PageSpec::StringNameParamPage).id}" }
           end
         end
       end
@@ -149,5 +225,43 @@ describe Crumble::Page do
     end
 
     res.should contain("Nested ID: 123")
+  end
+
+  it "by default only matches the derived root path" do
+    Crumble::PageSpec::TopLevelOnlyPage.match(Crumble::PageSpec::TopLevelOnlyPage.uri_path).should be_truthy
+    Crumble::PageSpec::TopLevelOnlyPage.match("#{Crumble::PageSpec::TopLevelOnlyPage.uri_path}/123").should be_falsey
+  end
+
+  it "allows overriding the derived root path via root_path macro" do
+    Crumble::PageSpec::CustomRootPathPage.root_path.should eq("/custom")
+    Crumble::PageSpec::CustomRootPathPage.match("/custom").should be_truthy
+    Crumble::PageSpec::CustomRootPathPage.match("/custom/123").should be_falsey
+  end
+
+  it "supports multiple path_param and nested_path segments" do
+    res = String.build do |io|
+      ctx = Crumble::Server::TestRequestContext.new(response_io: io, resource: "/multi/123/abc-9/edit/details")
+      Crumble::PageSpec::MultiParamPage.handle(ctx).should eq(true)
+      ctx.response.flush
+    end
+
+    res.should contain("account_id=123 slug=abc-9")
+    Crumble::PageSpec::MultiParamPage.match("/multi/123/abc_9/edit/details").should be_falsey
+  end
+
+  it "supports path_param names as Symbol or String literals" do
+    res = String.build do |io|
+      ctx = Crumble::Server::TestRequestContext.new(response_io: io, resource: Crumble::PageSpec::SymbolNameParamPage.uri_path(9))
+      Crumble::PageSpec::SymbolNameParamPage.handle(ctx).should eq(true)
+      ctx.response.flush
+    end
+    res.should contain("Symbol ID: 9")
+
+    res = String.build do |io|
+      ctx = Crumble::Server::TestRequestContext.new(response_io: io, resource: Crumble::PageSpec::StringNameParamPage.uri_path(11))
+      Crumble::PageSpec::StringNameParamPage.handle(ctx).should eq(true)
+      ctx.response.flush
+    end
+    res.should contain("String ID: 11")
   end
 end
--- src/page/page.cr
+++ src/page/page.cr
@@ -3,6 +3,84 @@ require "../server/view_handler"
 abstract class Crumble::Page
   include Crumble::Server::ViewHandler
 
+  @path_params : Hash(Symbol, String)?
+
+  module PathPart
+  end
+
+  struct NestedPathPart
+    include PathPart
+
+    getter segment : String
+
+    def initialize(segment : String)
+      @segment = segment.gsub(/\A\/+|\/+\z/, "")
+    end
+  end
+
+  struct ParamPathPart
+    include PathPart
+
+    getter name : Symbol
+    getter matcher : Regex
+
+    def initialize(name : Symbol, matcher : Regex)
+      @name = name
+      @matcher = matcher
+    end
+  end
+
+  PATH_PARTS = [] of PathPart
+
+  def self._path_parts : Array(PathPart)
+    PATH_PARTS
+  end
+
+  macro inherited
+    PATH_PARTS = [] of Crumble::Page::PathPart
+
+    def self._path_parts : Array(Crumble::Page::PathPart)
+      PATH_PARTS
+    end
+  end
+
+  macro root_path(path)
+    {% unless path.is_a?(StringLiteral) %}
+      {% raise "root_path expects a String literal" %}
+    {% end %}
+    {% unless path.starts_with?("/") %}
+      {% raise "root_path must start with /" %}
+    {% end %}
+
+    def self.root_path
+      {{path}}
+    end
+  end
+
+  macro path_param(name, matcher = /\d+/)
+    {% if name.is_a?(Call) && (name.receiver || name.args.size > 0) %}
+      {% raise "path_param name must be a simple identifier (got #{name})" %}
+    {% end %}
+
+    {% unless name.id.stringify =~ /\A[a-zA-Z_][a-zA-Z0-9_]*\z/ %}
+      {% raise "path_param name must be a valid method name (got #{name})" %}
+    {% end %}
+
+    PATH_PARTS << Crumble::Page::ParamPathPart.new({{name.id.symbolize}}, {{matcher}})
+
+    def {{name.id}} : String
+      path_params[{{name.id.symbolize}}]
+    end
+  end
+
+  macro nested_path(path)
+    {% unless path.is_a?(StringLiteral) %}
+      {% raise "nested_path expects a String literal" %}
+    {% end %}
+
+    PATH_PARTS << Crumble::Page::NestedPathPart.new({{path}})
+  end
+
   macro view(klass = nil, &blk)
     {% raise "Pass a view class or a block, not both" if klass && blk %}
     {% unless klass || blk %}{% raise "Provide a view class or block" %}{% end %}
@@ -78,23 +156,54 @@ abstract class Crumble::Page
   end
 
   def self.nested_path
-    ""
+    segments = [] of String
+    _path_parts.each do |part|
+      next unless part.is_a?(NestedPathPart)
+      segments << part.segment
+    end
+    segments.empty? ? "" : "/" + segments.join("/")
   end
 
-  def self.uri_path(id = nil)
-    path = root_path
-    if id
-      path += "/#{id}"
-      path += nested_path
+  def self.uri_path(*params)
+    param_values = params.map(&.to_s)
+    segments = root_path.split('/').reject(&.empty?)
+
+    param_index = 0
+    _path_parts.each do |part|
+      case part
+      when NestedPathPart
+        segments << part.segment
+      when ParamPathPart
+        value = param_values[param_index]? || raise ArgumentError.new("Missing path param '#{part.name}' for #{self}")
+        segments << value
+        param_index += 1
+      end
     end
-    path
+
+    if param_index != param_values.size
+      raise ArgumentError.new("Too many path params for #{self}: expected #{param_index}, got #{param_values.size}")
+    end
+
+    "/" + segments.join("/")
   end
 
   def self.uri_path_matcher
-    if nested_path.empty?
-      /^#{root_path}(\/|\/(\d+))?$/
+    root_segments = root_path.split('/').reject(&.empty?)
+    segment_patterns = root_segments.map { |seg| Regex.escape(seg) }
+
+    _path_parts.each do |part|
+      case part
+      when NestedPathPart
+        segment_patterns << Regex.escape(part.segment)
+      when ParamPathPart
+        segment_patterns << "(#{part.matcher.source})"
+      end
+    end
+
+    if segment_patterns.empty?
+      /^\/$/
     else
-      /^#{root_path}(\/|\/(\d+)(#{nested_path})?)?$/
+      Regex.new("^/" + segment_patterns.join("/") + "/?$")
     end
   end
 
@@ -126,7 +235,7 @@ abstract class Crumble::Page
   end
 
   def id?
-    self.class.match(ctx.request.path).try { |m| m[2]?.try(&.to_i64) }
+    path_params[:id]?.try(&.to_i64?)
   end
 
   def id
@@ -134,7 +243,7 @@ abstract class Crumble::Page
   end
 
   def nested?
-    self.class.nested_path.size > 0
+    self.class._path_parts.size > 0
   end
 
   def top_level?
@@ -147,5 +256,22 @@ abstract class Crumble::Page
 
   def window_title : String?
     nil
+  end
+
+  protected def path_params : Hash(Symbol, String)
+    @path_params ||= begin
+      params = {} of Symbol => String
+      match = self.class.match(ctx.request.path)
+      return params unless match
+
+      capture_index = 1
+      self.class._path_parts.each do |part|
+        if part.is_a?(ParamPathPart)
+          params[part.name] = match[capture_index]
+          capture_index += 1
+        end
+      end
+      params
+    end
   end
 end
