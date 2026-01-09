293fc9919756a5b2f6f7b845eb2864113bc5ebb8
Stefan Bilharz <stefan@sbsoftware.de>
2026-01-09 22:26:09 +0100

    CR-102 Add option to skip form labels or set their caption

--- spec/form_spec.cr
+++ spec/form_spec.cr
@@ -1,30 +1,66 @@
 require "./spec_helper"
 
 class Crumble::FormSpec
-  class MyForm < Crumble::Form
+  class DefaultForm < Crumble::Form
     field a : String, type: :hidden
     field b : String?, type: :text
+  end
+
+  class LabeledForm < Crumble::Form
+    field a : String, type: :hidden, label: "Custom A"
+    field b : String?, type: :text, label: nil
     field c : String
-    field d : Bool, type: :hidden
+  end
+
+  class CustomDefaultLabelForm < Crumble::Form
+    field a : String
+    field b : String
+
+    macro default_label_caption(field)
+      "I18n.#{ {{field}} }"
+    end
   end
 
   class EmptyForm < Crumble::Form
   end
 
-  describe "MyForm#to_html" do
+  describe "DefaultForm#to_html" do
     it "should return the correct HTML" do
       expected = <<-HTML.squish
-      <label for="crumble--form-spec--my-form--a-field-id">A</label>
-      <input id="crumble--form-spec--my-form--a-field-id" type="hidden" name="a" value="Blah">
-      <label for="crumble--form-spec--my-form--b-field-id">B</label>
-      <input id="crumble--form-spec--my-form--b-field-id" type="text" name="b" value="">
-      <label for="crumble--form-spec--my-form--c-field-id">C</label>
-      <input id="crumble--form-spec--my-form--c-field-id" type="text" name="c" value="foo">
-      <label for="crumble--form-spec--my-form--d-field-id">D</label>
-      <input id="crumble--form-spec--my-form--d-field-id" type="hidden" name="d" value="true">
+      <label for="crumble--form-spec--default-form--a-field-id">A</label>
+      <input id="crumble--form-spec--default-form--a-field-id" type="hidden" name="a" value="Blah">
+      <label for="crumble--form-spec--default-form--b-field-id">B</label>
+      <input id="crumble--form-spec--default-form--b-field-id" type="text" name="b" value="">
       HTML
 
-      MyForm.new(a: "Blah", b: nil, c: "foo", d: true).to_html.should eq(expected)
+      DefaultForm.new(a: "Blah", b: nil).to_html.should eq(expected)
+    end
+  end
+
+  describe "LabeledForm#to_html" do
+    it "should support custom and nil labels" do
+      expected = <<-HTML.squish
+      <label for="crumble--form-spec--labeled-form--a-field-id">Custom A</label>
+      <input id="crumble--form-spec--labeled-form--a-field-id" type="hidden" name="a" value="Blah">
+      <input id="crumble--form-spec--labeled-form--b-field-id" type="text" name="b" value="">
+      <label for="crumble--form-spec--labeled-form--c-field-id">C</label>
+      <input id="crumble--form-spec--labeled-form--c-field-id" type="text" name="c" value="foo">
+      HTML
+
+      LabeledForm.new(a: "Blah", b: nil, c: "foo").to_html.should eq(expected)
+    end
+  end
+
+  describe "CustomDefaultLabelForm#to_html" do
+    it "should allow overriding default label behavior" do
+      expected = <<-HTML.squish
+      <label for="crumble--form-spec--custom-default-label-form--a-field-id">I18n.a</label>
+      <input id="crumble--form-spec--custom-default-label-form--a-field-id" type="text" name="a" value="x">
+      <label for="crumble--form-spec--custom-default-label-form--b-field-id">I18n.b</label>
+      <input id="crumble--form-spec--custom-default-label-form--b-field-id" type="text" name="b" value="y">
+      HTML
+
+      CustomDefaultLabelForm.new(a: "x", b: "y").to_html.should eq(expected)
     end
   end
 
--- src/form.cr
+++ src/form.cr
@@ -19,14 +19,40 @@ module Crumble
       {% end %}
     end
 
-    macro field(type_decl, *, type = nil)
-      @[Field(type: {{(type || :text).id.symbolize}})]
+    macro field(type_decl, *, type = nil, label = :__crumble_default__)
+      {% if label == :__crumble_default__ %}
+        @[Field(type: {{(type || :text).id.symbolize}})]
+      {% else %}
+        @[Field(type: {{(type || :text).id.symbolize}}, label: {{label}})]
+      {% end %}
       {% if type_decl.type.resolve.nilable? %}
         @[Nilable]
       {% end %}
       getter {{type_decl.var}} : {{type_decl.type}}?
 
       css_id {{type_decl.var.id.stringify.camelcase.id}}FieldId
+    end
+
+    # Returns the label caption for a field when no explicit `label:` was passed
+    # to the `field` macro. Override to customize default label behavior (e.g.
+    # perform an I18n lookup).
+    macro default_label_caption(field)
+      {{field}}.to_s.capitalize
+    end
+
+    protected def label_caption_for(field : Symbol) : String?
+      {% for ivar in @type.instance_vars.select { |iv| iv.annotation(Field) } %}
+        if field == :{{ivar.name}}
+          {% ann = ivar.annotation(Field) %}
+          {% if ann.named_args.has_key?(:label) %}
+            return {{ann.named_args[:label]}}
+          {% else %}
+            return default_label_caption(:{{ivar.name}})
+          {% end %}
+        end
+      {% end %}
+
+      default_label_caption(field)
     end
 
     def valid?
@@ -68,9 +94,9 @@ module Crumble
 
     ToHtml.instance_template do
       {% for ivar in @type.instance_vars.select { |iv| iv.annotation(Field) } %}
-        label {{@type}}::{{ivar.name.stringify.camelcase.id}}FieldId do
-          {{ivar.name.stringify}}.capitalize
-        end; input {{@type}}::{{ivar.name.stringify.camelcase.id}}FieldId, type: {{ivar.annotation(Field)[:type]}}, name: {{ivar.name.stringify}}, value: {{ivar.name.id}}.to_s
+        if %label_caption = label_caption_for(:{{ivar.name}}); label {{@type}}::{{ivar.name.stringify.camelcase.id}}FieldId do
+          %label_caption
+        end; end; input {{@type}}::{{ivar.name.stringify.camelcase.id}}FieldId, type: {{ivar.annotation(Field)[:type]}}, name: {{ivar.name.stringify}}, value: {{ivar.name.id}}.to_s
       {% end %}
     end
   end
