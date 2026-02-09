8a0a81aff81c63921f5e5298abbfd348255ff8bc
Stefan Bilharz <stefan@sbsoftware.de>
2026-02-09 22:23:00 +0100

    CR-121 Require base classes early

--- src/cli/templates/environment.cr
+++ src/cli/templates/environment.cr
@@ -1,4 +1,13 @@
 require "crumble"
+
+# Load base application types first so files in nested folders can reference them.
+require "./crumble/session"
+require "./crumble/request_context"
+require "./views/application_layout"
+require "./pages/application_page"
+require "./resources/application_resource"
+require "./styles/application_style"
+
 require "./crumble/**"
 require "./models/**"
 require "./views/**"
